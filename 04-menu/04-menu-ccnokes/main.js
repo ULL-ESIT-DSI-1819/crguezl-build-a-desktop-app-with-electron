0ef91c6709b65de4668443846bb0f9374243b733
Casiano Rodriguez-Leon <crguezl@ull.edu.es>
2018-07-27 13:57:31 +0100

    04-menu-ccnokes-main.js-globalsortcut-register-example

--- 04-menu/04-menu-ccnokes/main.js
+++ 04-menu/04-menu-ccnokes/main.js
@@ -1,4 +1,4 @@
-const { app, BrowserWindow } = require('electron');
+const { app, BrowserWindow, globalShortcut } = require('electron');
 const path = require('path');
 const debug = /--debug/.test(process.argv[2])
 
@@ -20,6 +20,27 @@ app.on('ready', () => {
       require('devtron').install();
     }
     console.log('start work main pid = ', process.pid);
+
+    // Register a 'CommandOrControl+X' shortcut listener.
+    const ret = globalShortcut.register('CommandOrControl+X', () => {
+      console.log('CommandOrControl+X is pressed')
+    })
+
+     if (!ret) {
+       console.log('registration failed')
+     }
+
+     // Check whether a shortcut is registered.
+     console.log(globalShortcut.isRegistered('CommandOrControl+X'))
   });
+
+  app.on('will-quit', () => {
+    // Unregister a shortcut.
+    globalShortcut.unregister('CommandOrControl+X')
+
+    // Unregister all shortcuts.
+    globalShortcut.unregisterAll()
+  })
+
   setMainMenu();
 });
