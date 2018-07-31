3a26e82d8fbe91b72c893de73affbb0686737c98
Casiano Rodriguez-Leon <crguezl@ull.edu.es>
2018-07-31 13:57:10 +0100

    index.js

--- gopinav-10-shell/index.js
+++ gopinav-10-shell/index.js
@@ -1,7 +1,7 @@
 const shell = require('electron').shell;
 const openBtn = document.getElementById('openBtn');
 openBtn.addEventListener('click', function (event) {
-  shell.showItemInFolder('images');
+  shell.showItemInFolder('images/gv.jpeg');
   shell.openItem('images/gv.jpeg');
   shell.openExternal('http://electron.atom.io');
 })
