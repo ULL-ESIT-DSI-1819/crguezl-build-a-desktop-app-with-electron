1992002bd8a029b6ff4da4ed04844debb8db8b5e
Casiano Rodriguez-Leon <crguezl@ull.edu.es>
2018-08-16 21:56:59 +0100

    refactorizing

--- gopinav-05-quote-widget/index.js
+++ gopinav-05-quote-widget/index.js
@@ -1,25 +1,21 @@
 const request = require("request");
 const quote = document.getElementById('quote');
 
-request("http://quotesondesign.com/wp-json/posts?filter[orderby]=rand&filter[posts_per_page]=1",
-  (err, response, body) => {
-    let bodyJson = JSON.parse(body)[0];
-    console.log(bodyJson);
-    let randomQuote = bodyJson.content;
-    quote.innerHTML = randomQuote;
-  }
-);
+// See https://quotesondesign.com/api-v4-0/
+function getQuote() {
+  request("http://quotesondesign.com/wp-json/posts?filter[orderby]=rand&filter[posts_per_page]=1",
+    (err, response, body) => {
+      let bodyJson = JSON.parse(body)[0];
+      console.log(bodyJson);
+      let randomQuote = bodyJson.content;
+      quote.innerHTML = randomQuote;
+    }
+  );
+}
+
+getQuote();
 
 setInterval(
-  () => {
-    request("http://quotesondesign.com/wp-json/posts?filter[orderby]=rand&filter[posts_per_page]=1",
-      (err, response, body) => {
-        let bodyJson = JSON.parse(body)[0];
-        console.log(bodyJson);
-        let randomQuote = bodyJson.content;
-        quote.innerHTML = randomQuote;
-      }
-    );
-  },
+  getQuote,
   6000 // 6 seconds
 );
